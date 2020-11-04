0ad160437de444fd2ed9edd3487757ccabf48c58
Anel <a.yerzhanova@devir.kz>
2020-11-04 20:02:04 +0600

    first one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TasksViewComponent } from './tasks-view/tasks-view.component';
 
+
+import { TaskService} from "./task.service";
+import { AddTaskComponent } from './add-task/add-task.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksViewComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
